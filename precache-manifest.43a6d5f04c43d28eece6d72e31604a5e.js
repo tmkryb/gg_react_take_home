637737da02a3b18e32c3fbbadf1f1fe3e6ee94c5
tmkryb <tmkryb@gmail.com>
2020-10-08 12:14:28 +0200

    Updates

--- precache-manifest.43a6d5f04c43d28eece6d72e31604a5e.js
+++ precache-manifest.43a6d5f04c43d28eece6d72e31604a5e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "14e2f7aa9070c1207416f301c9f9d092",
+    "revision": "26bd6838856885c5b08da5f4bc289d9a",
     "url": "/gg_react_take_home/index.html"
   },
   {
-    "revision": "b5f9cebdcb78f5b67d63",
+    "revision": "bd62651233b63686727d",
     "url": "/gg_react_take_home/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "0cf97bca0e0087a0f7c1",
+    "revision": "b3f6a7142fe593cee1c9",
     "url": "/gg_react_take_home/static/css/main.95bc8d7d.chunk.css"
   },
   {
-    "revision": "b5f9cebdcb78f5b67d63",
-    "url": "/gg_react_take_home/static/js/2.bf708663.chunk.js"
+    "revision": "bd62651233b63686727d",
+    "url": "/gg_react_take_home/static/js/2.d2c0dd0b.chunk.js"
   },
   {
     "revision": "b046e08719770a3ec798a1e542ce3900",
-    "url": "/gg_react_take_home/static/js/2.bf708663.chunk.js.LICENSE.txt"
+    "url": "/gg_react_take_home/static/js/2.d2c0dd0b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0cf97bca0e0087a0f7c1",
-    "url": "/gg_react_take_home/static/js/main.0b1eaf91.chunk.js"
+    "revision": "b3f6a7142fe593cee1c9",
+    "url": "/gg_react_take_home/static/js/main.d42ca5b7.chunk.js"
   },
   {
     "revision": "f69b9631acc4860d64e1",
