de8d0b8ad7a54301526334dc8843019a385ccb5f
tmkryb <tmkryb@gmail.com>
2020-10-08 11:47:18 +0200

    Updates

---
+++ precache-manifest.003cc1a8dd15c0289a971864907f832a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36996e4015a61844f2a7ead5233e8210",
+    "url": "/gg_react_take_home/index.html"
+  },
+  {
+    "revision": "bd62651233b63686727d",
+    "url": "/gg_react_take_home/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f94586fc0d798dc645bf",
+    "url": "/gg_react_take_home/static/css/main.95bc8d7d.chunk.css"
+  },
+  {
+    "revision": "bd62651233b63686727d",
+    "url": "/gg_react_take_home/static/js/2.d2c0dd0b.chunk.js"
+  },
+  {
+    "revision": "b046e08719770a3ec798a1e542ce3900",
+    "url": "/gg_react_take_home/static/js/2.d2c0dd0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f94586fc0d798dc645bf",
+    "url": "/gg_react_take_home/static/js/main.68d0f31b.chunk.js"
+  },
+  {
+    "revision": "f69b9631acc4860d64e1",
+    "url": "/gg_react_take_home/static/js/runtime-main.42d4d1f8.js"
+  }
+]);
